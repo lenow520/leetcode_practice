364adc2dc2c5906b00969797364c6132f7e96eea
Tzu-Chien Huang <68072099+lenow520@users.noreply.github.com>
2022-12-17 18:07:23 +0800

    Add files via upload
    completed LIS solutions

--- src/script/longestincseq/solution.py
+++ src/script/longestincseq/solution.py
@@ -1,2 +1,32 @@
-def LIS():
-    pass
+def LIS(seq):
+    n = len(seq)
+    if n == 0:
+        return 0
+    
+    LISlen = [1]*n
+    max_value = 1
+    for i in range(n):
+        headmax = 1
+        for j in range(i):
+            if seq[i]>seq[j] and headmax <= LISlen[j]:
+                headmax = LISlen[j]+1
+        LISlen[i] = headmax
+        if LISlen[i]>max_value:
+
+            max_value = LISlen[i]
+            #print(max_value)
+    print(LISlen)
+    return max_value
+
+
+inputseq =  [10,9,2,5,3,7,101,18]
+ans = LIS(inputseq)
+print(ans)
+            
+inputseq =   [1,4,2,4,2,3]
+ans = LIS(inputseq)
+print(ans)
+
+inputseq =   [1,1,1,2]
+ans = LIS(inputseq)
+print(ans)
---
+++ src/script/longestincseq/solution2.py
@@ -0,0 +1,45 @@
+def binarysearch(target, lst):
+    start = 0
+    end = len(lst) - 1
+    while start <= end:
+        middle = int((start + end)/ 2)
+        midpoint = lst[middle]
+        if midpoint < target:
+            start = middle + 1
+        else:
+            end = middle - 1
+    return start
+
+
+def LIS(seq):
+    n = len(seq)
+    if n == 0:
+        return 0
+    
+    magic = [seq[0]]
+    for i in range(1, n):
+        if magic[-1] < seq[i]:
+            magic.append(seq[i])
+        elif magic[0] > seq[i]:
+            magic[0] = seq[i]
+        else:
+            target = seq[i]
+            index = binarysearch(target, magic)
+            left = magic[:index]
+            magic = left+[target]
+    
+    max_value = len(magic)       
+    return max_value
+
+
+inputseq =  [10,9,2,5,3,7,101,18]
+ans = LIS(inputseq)
+print(ans)
+            
+inputseq =   [1,4,2,4,2,3]
+ans = LIS(inputseq)
+print(ans)
+
+inputseq =   [1,1,1,2]
+ans = LIS(inputseq)
+print(ans)
